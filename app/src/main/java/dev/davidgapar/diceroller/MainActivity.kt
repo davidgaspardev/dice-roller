c9f45e14412090fb7f0a37d4226feec8f303682a
David Gaspar <davidgaspar.dev@gmail.com>
2021-09-04 16:48:35 -0300

    Adopting good coding practices

--- app/src/main/java/dev/davidgapar/diceroller/MainActivity.kt
+++ app/src/main/java/dev/davidgapar/diceroller/MainActivity.kt
@@ -2,12 +2,10 @@ package dev.davidgapar.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     private val LOG_TAG = "Main Activity"
 
@@ -16,21 +14,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener(this)
+        rollButton.setOnClickListener { rollDice() }
     }
 
-    override fun onClick(v: View?) {
-        if(v !== null) {
-            when(v.id) {
-                R.id.button -> {
-                    val textView: TextView = findViewById(R.id.textView)
-                    textView.text = Dice(6).roll().toString()
-                }
-                else -> {
-                    Log.i(LOG_TAG, "Don't have onClick event to id: ${v.id}")
-                }
-            }
-        }
+    private fun rollDice() {
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = Dice(6).roll().toString()
     }
 
     class Dice(private val numSides: Int) {
